0b54fa16a4884a9428a4c10a4c7e529444eb3b93
naranjito72 <theclick36@gmail.com>
2021-05-05 01:14:36 +0200

    tree of project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StarshipsListComponent } from './starships/starships-list/starships-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch:'full' },
+  { path: 'starships', component: StarshipsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
