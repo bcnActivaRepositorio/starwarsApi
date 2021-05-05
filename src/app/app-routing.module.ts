deb74be4d08992686faf5b9ba4580aec98681f94
naranjito72 <theclick36@gmail.com>
2021-05-05 09:38:58 +0200

    all tree completed/routing stablished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { StarshipsListComponent } from './starships/starships-list/starships-list.component';
 
+import { StarshipsListComponent } from './starships/starships-list/starships-list.component';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+
+// routes
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch:'full' },
-  { path: 'starships', component: StarshipsListComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'starships', component: StarshipsListComponent},
+  { path: '404', component: PageNotFoundComponent},
+  // Path ** MUST be always the last route
+ { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// made components
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
